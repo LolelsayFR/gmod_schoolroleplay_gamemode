8a10e6f9f0d9115d96620a07835687aca0c406e1
Emilien <lolelsay@vps-72a80f6b.vps.ovh.net>
2025-05-03 22:39:31 +0000

    Add my workshop files

---
+++ gamemode/init.lua
@@ -0,0 +1,10 @@
+hook.Run("DarkRPStartedLoading")
+
+GM.Version = "1.0.0"
+GM.Name = "SchoolRoleplay"
+GM.Author = "Derived fork by Lolelsay"
+
+DeriveGamemode("darkrp")
+DEFINE_BASECLASS("gamemode_darkrp")
+
+GM.DarkRP = BaseClass
